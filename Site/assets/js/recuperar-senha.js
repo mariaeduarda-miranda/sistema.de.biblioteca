f30dcd8b9fb544b0df2d6a387770b0d5e1488acf
MysticWendel <soares.wendeljr@gmail.com>
2025-04-12 01:50:28 -0300

    database e prototipo de telas

---
+++ Site/assets/js/recuperar-senha.js
@@ -0,0 +1,9 @@
+function verificarEmail (){
+    email = document.getElementById("email").value;
+    loading = document.getElementById("loading");
+    loading.style.visibility = "visible";
+    
+
+    for(let i = 0; i < .length)
+
+}
